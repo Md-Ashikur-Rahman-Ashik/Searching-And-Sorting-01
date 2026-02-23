59204ddc4fa59bc55e244223bc0ee2d6c342ce5a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2026-02-23 18:52:38 +0600

    Solved Distinct Numbers problem - L

---
+++ Distinct_Numbers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <class T>
+using pbds = tree<T, null_type,
+                  less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        mp[v[i]]++;
+    }
+
+    int size = mp.size();
+    cout << size << '\n';
+
+    return 0;
+}
